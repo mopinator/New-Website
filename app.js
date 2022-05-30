0ad8f6e4358277fe8be399f13a60d0df47d42d58
Simon Li <li.simon021@gmail.com>
2022-05-30 16:16:03 +1200

    updated 30_05

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const checkbox = document.getElementById("checkbox");
+const checkbox = document.querySelector(".checkbox");
 
 checkbox.addEventListener("change", () => {
 	document.body.classList.toggle("light");
